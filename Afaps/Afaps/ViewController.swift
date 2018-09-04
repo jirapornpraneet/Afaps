6daf656d4035401025ec35af1c99fea26475ad75
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-09-04 14:31:54 +0700

    initial commit

---
+++ Afaps/Afaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Afaps
+//
+//  Created by Jiraporn Praneet on 4/9/2561 BE.
+//  Copyright © 2561 InformatixPlusCompanyLimited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
